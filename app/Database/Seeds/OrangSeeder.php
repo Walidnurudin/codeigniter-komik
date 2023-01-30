4351b3d05b73b1539b4820e14a8e7aa3c2b7ac4a
Walidnurudin <walidnurudin47@gmail.com>
2023-01-30 14:02:15 +0700

    data with faker

--- app/Database/Seeds/OrangSeeder.php
+++ app/Database/Seeds/OrangSeeder.php
@@ -9,17 +9,25 @@ class OrangSeeder extends Seeder
 {
     public function run()
     {
-        $data = [
-            'name' => 'paqu',
-            'address'    => 'jln 123',
-            'created_at' => Time::now(),
-            'updated_at' => Time::now(),
-        ];
+
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 100; $i++) {
+            $data = [
+                'name' => $faker->name,
+                'address'    => $faker->address,
+                'created_at' => Time::createFromTimestamp($faker->unixTime()),
+                'updated_at' => Time::now(),
+            ];
+
+            $this->db->table('orang')->insert($data);
+        }
+
 
         // Simple Queries
         // $this->db->query('INSERT INTO users (username, email) VALUES(:username:, :email:)', $data);
 
-        // Using Query Builder
-        $this->db->table('orang')->insert($data);
+        // Using Query Builder (insertBatch for many data)
+        // $this->db->table('orang')->insert($data);
     }
 }
